be37ce4a2991d7594eddcd67b617f2da9f980892
komal-sharma19 <145161424+komal-sharma19@users.noreply.github.com>
2025-04-04 16:43:44 +0530

    code added

---
+++ client/build/precache-manifest.f1bc16dd43acb0987f050982722ec199.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fadf9dcbbb354a2223d4d36c183fa98f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a09999d9a078e432bc41",
+    "url": "/static/css/2.2a42abf6.chunk.css"
+  },
+  {
+    "revision": "8c76b8cdee888ebb18a7",
+    "url": "/static/css/main.75f2378f.chunk.css"
+  },
+  {
+    "revision": "a09999d9a078e432bc41",
+    "url": "/static/js/2.85c46eec.chunk.js"
+  },
+  {
+    "revision": "5c308587bbb541708688",
+    "url": "/static/js/3.6fc501d6.chunk.js"
+  },
+  {
+    "revision": "8c76b8cdee888ebb18a7",
+    "url": "/static/js/main.9f37d252.chunk.js"
+  },
+  {
+    "revision": "4cd9830544a5cf7d1d11",
+    "url": "/static/js/runtime~main.310794f0.js"
+  }
+]);
---
+++ routes/transactionRoutes.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const {
+  addTransaction,
+  getAllTransaction,
+  editTransaction,
+  deleteTransaction,
+} = require("../controllers/transactionCtrl");
+
+//router object
+const router = express.Router();
+
+//routes
+//add transaction POST Method
+router.post("/add-transaction", addTransaction);
+//Edit transaction POST Method
+router.post("/edit-transaction", editTransaction);
+//Delete transaction POST Method
+router.post("/delete-transaction", deleteTransaction);
+
+//get transactions
+router.post("/get-transaction", getAllTransaction);
+
+module.exports = router;
